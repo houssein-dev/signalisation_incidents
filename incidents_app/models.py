7e5e47b63236ee70dca511a1cc05755ea94fca65
houcein-dev <houceinhmd@gmail.com>
2025-04-19 17:55:12 -0700

    activvite 2

--- incidents_app/models.py
+++ incidents_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Catagorie(models.Model):
+    Catagories_choices=(('incendie','incendie'),
+                        ('accident', 'accident'),
+                        ('poublles','poublles'),
+                        ('fuites d\'eau','fuites d\'eau'))
+    id_catagorie=models.AutoField(primary_key=True),
+    nome=models.CharField(choices=Catagories_choices , max_length=12 , default='incendie')
+    
+    
+    
+class Incident(models.Model):
+    
+    etat_choices=(('En attante','En attante'),('traitee','traitee'))
+    photo_incident = models.ImageField(upload_to='media/images_incidents'),
+    descriptions_text= models.TextField(max_length=400),
+    descriptions_vocal=models.FileField(upload_to='media/audio_incidents'),
+    etat=models.CharField(max_length=12 , choices=etat_choices)
+    date_creation=models.DateTimeField(auto_now_add=True)
+    catagorie=models.ForeignKey(Catagorie ,on_delete=models.SET_DEFAULT ,default='non determine')
